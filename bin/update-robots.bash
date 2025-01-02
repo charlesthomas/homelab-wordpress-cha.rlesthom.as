ed27259f44f283ecc2d67255ffe9975642932569
Charles Thomas <ch@rlesthom.as>
2025-01-02 17:52:50 -0500

    feat: `bin/update-robots.bash`
    403s all bots from https://github.com/ai-robots-txt/ai.robots.txt

---
+++ bin/update-robots.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+out=ingresses.temp
+
+url=https://github.com/ai-robots-txt/ai.robots.txt/releases/latest/download/robots.txt
+
+bots="$(curl -sL $url | grep User-agent | cut -f 2 -d : | tr -d ' ' | tr \\n \| | rev | cut -f 2- -d \| | rev)"
+
+cat resources/ingresses.yaml | while IFS= read line; do
+    if [[ "${line}" =~ "\$http_user_agent" ]]; then
+        echo "      if (\$http_user_agent ~* \"(${bots})\"){" >> $out
+    else
+        echo "${line}" >> $out
+    fi
+done
+
+mv $out resources/ingresses.yaml
